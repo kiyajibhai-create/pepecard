2699739520894bd55654d383e16da8322d167920
Joy-Boy-999s <pk5082093@gmail.com>
2026-04-15 22:18:57 +0530

    #fix

--- pepecard-nextjs/app/robots.ts
+++ pepecard-nextjs/app/robots.ts
@@ -2,24 +2,50 @@ import type { MetadataRoute } from 'next'
 import { SITE_CONFIG } from '@/lib/seo'
 
 export default function robots(): MetadataRoute.Robots {
-  const base = SITE_CONFIG.url // https://www.pepecard.store
+  const base = SITE_CONFIG.url
 
   return {
     rules: [
       {
-        userAgent: '*',
-        allow: ['/', '/login', '/register', '/news', '/sitemap.xml', '/robots.txt'],
-        disallow: ['/api/', '/_next/', '/favicon.ico'],
-      },
-      {
         userAgent: 'Googlebot',
-        allow: ['/', '/login', '/register', '/news', '/sitemap.xml', '/robots.txt',
-          '/icon.png', '/apple-icon.png', '/og-image.png', '/rectangle-logo.png'],
-        disallow: ['/api/', '/_next/'],
+        allow: [
+          '/',
+          '/_next/static/',
+          '/login',
+          '/register',
+          '/news',
+          '/sitemap.xml',
+          '/robots.txt',
+          '/icon.png',
+          '/apple-icon.png',
+          '/og-image.png',
+          '/rectangle-logo.png',
+          '/assets/',
+        ],
+        disallow: ['/api/', '/_next/server/'],
       },
       {
         userAgent: 'Googlebot-Image',
-        allow: ['/og-image.png', '/icon.png', '/apple-icon.png', '/rectangle-logo.png', '/assets/'],
+        allow: [
+          '/og-image.png',
+          '/icon.png',
+          '/apple-icon.png',
+          '/rectangle-logo.png',
+          '/assets/',
+        ],
+      },
+      {
+        userAgent: '*',
+        allow: [
+          '/',
+          '/_next/static/',
+          '/login',
+          '/register',
+          '/news',
+          '/sitemap.xml',
+          '/robots.txt',
+        ],
+        disallow: ['/api/', '/_next/server/'],
       },
     ],
     sitemap: [
